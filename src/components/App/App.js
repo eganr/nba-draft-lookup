7f25c6048a7208f93301745b9535278c051a72fc
Ryan Egan <regan@konradgroup.com>
2015-12-08 21:42:32 -0500

    Added React Starter Kit

---
+++ src/components/App/App.js
@@ -0,0 +1,61 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import React, { Component, PropTypes } from 'react';
+import emptyFunction from 'fbjs/lib/emptyFunction';
+import s from './App.scss';
+import Header from '../Header';
+import Feedback from '../Feedback';
+import Footer from '../Footer';
+
+class App extends Component {
+
+  static propTypes = {
+    context: PropTypes.shape({
+      insertCss: PropTypes.func,
+      onSetTitle: PropTypes.func,
+      onSetMeta: PropTypes.func,
+      onPageNotFound: PropTypes.func,
+    }),
+    children: PropTypes.element.isRequired,
+    error: PropTypes.object,
+  };
+
+  static childContextTypes = {
+    insertCss: PropTypes.func.isRequired,
+    onSetTitle: PropTypes.func.isRequired,
+    onSetMeta: PropTypes.func.isRequired,
+    onPageNotFound: PropTypes.func.isRequired,
+  };
+
+  getChildContext() {
+    const context = this.props.context;
+    return {
+      insertCss: context.insertCss || emptyFunction,
+      onSetTitle: context.onSetTitle || emptyFunction,
+      onSetMeta: context.onSetMeta || emptyFunction,
+      onPageNotFound: context.onPageNotFound || emptyFunction,
+    };
+  }
+
+  componentWillMount() {
+    this.removeCss = this.props.context.insertCss(s);
+  }
+
+  componentWillUnmount() {
+    this.removeCss();
+  }
+
+  render() {
+    return !this.props.error ? (
+      <div>
+        <Header />
+        {this.props.children}
+        <Feedback />
+        <Footer />
+      </div>
+    ) : this.props.children;
+  }
+
+}
+
+export default App;
